c3cb492aed8140e3a58fb09212d236689a677437
Ricardo Madriz <richin13@gmail.com>
2017-04-16 22:11:39 -0600

    Add experimental version of 'Draw a square' :fire:

---
+++ scripts/draw_square.ext.py
@@ -0,0 +1,25 @@
+from nxt.motor import PORT_A, PORT_B, PORT_C
+from .helpers.robot import Robot, SERVO_NICE
+
+
+def main():
+    robot = Robot(debug=True, verbose=True)
+
+    # Motors
+    robot.init_synchronized_motors(PORT_A, PORT_C)
+    robot.init_servo(PORT_B)
+
+    robot.set_servo(SERVO_NICE)
+    sides = 4  # Spoiler alert! It's a square.
+    side_length = 10  # In centimeters
+    angle = 90
+
+    for _ in range(sides):
+        robot.move_forward(dist=side_length)
+        robot.turn_right(power=50, degrees=(180 - angle))
+
+    print('[DONE] Won\'t do anything else.')
+
+
+if __name__ == '__main__':
+    main()
