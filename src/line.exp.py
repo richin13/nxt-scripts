ad0f28ae735777e5154c679e62704daa42e27613
Ricardo Madriz <richin13@gmail.com>
2017-05-05 08:27:30 -0600

    Improve calibration process

--- src/line.exp.py
+++ src/line.exp.py
@@ -1,13 +1,12 @@
 from nxt import locator
 from nxt.sensor.common import PORT_2, PORT_3
 from nxt.motor import PORT_A, PORT_B, PORT_C
-from .helpers import Robot, SERVO_NICE, ON, OFF
-from .helpers import normalize
-
+from scripts.helpers import Robot, SERVO_NICE, ON, OFF
+from scripts.helpers import normalize
+from time import sleep
 
 def main():
-    brick = locator.find_one_brick()  # USB connection
-    robot = Robot(brick)
+    robot = Robot(debug=True, verbose=True, power=20)
 
     # Sensors
     light = robot.init_light_sensor(PORT_2)
@@ -18,19 +17,20 @@ def main():
     robot.init_synchronized_motors(PORT_A, PORT_C)
 
     # Initial setup
-    robot.set_servo(SERVO_NICE)  # It's called nice because the servo movement looks freaking awesome
+    robot.set_servo(SERVO_NICE, power=50)  # It's called nice because the servo movement looks freaking awesome
     robot.turn_light_sensor(ON)
 
     # Light sensor calibration
-    black, white = robot.calibrate_light()
+    black, white = robot.calibrate_light(interactive=True)
 
     # Thresholds
     lower = 4 * (10 ** -1)
     upper = 9 * (10 ** -1)
 
     def until(light_sensor, touch_sensor, **kwargs):
-        return normalize(light_sensor.get_lightness(), black, white) < upper or touch_sensor.is_pressed()
+        return normalize(light_sensor.get_lightness(), black, white) > upper or touch_sensor.is_pressed()
 
+    sleep(1)
     robot.move_forward(until=until, until_args=(light, touch))
     while robot.running:
         light_level = normalize(light.get_lightness(), black, white)
@@ -41,15 +41,17 @@ def main():
             if light_level_tmp > light_level:
                 robot.debug('Right turn didn\'t improve course. Turning left...')
                 robot.turn_left(8, 30)
-
-        robot.move_forward(until=until, until_args=(light, touch))
+        if not until(light, touch):
+            robot.move_forward(until=until, until_args=(light, touch))
 
     # WARNING: From here, the code is even more experimental.
     # I encourage you to consider this some sort of fancy pseudo-code.
     if touch.is_pressed():
         robot.debug('Found an obstacle.')
-        robot.turn_right(50, 45)
-        robot.move_forward(dist=10, power=100)  # Dist is given in cm
+        robot.turn_right(60, 270)
+        robot.move_backwards(dist=30)
+        robot.turn_left(power=50, degrees=270 - 45)
+        robot.move_forward(dist=50, power=100)  # Dist is given in cm
 
     robot.turn_light_sensor(OFF)
 
